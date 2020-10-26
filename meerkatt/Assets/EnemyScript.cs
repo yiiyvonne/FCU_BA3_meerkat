6da2b86412b9877b9ee86aaa2dcbb32a69aa1984
yvonne <d0686014@o365.fcu.edu.tw>
2020-10-26 12:21:57 +0800

    meerkatt

---
+++ meerkatt/Assets/EnemyScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private int enemyDistance = 0;
+    private int enemyCount = 10;
+
+    private string[] enemies = new string[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            //EnemySearch();
+            EnemyDestruction();
+            EnemyRoster();
+
+        }
+    }
+
+    void EnemySearch()
+    {
+        enemyDistance = Random.Range(1, 10);
+
+        if (enemyDistance >= 8)
+        {
+            print("enemy is far away");
+        }
+
+        if (enemyDistance >= 4 && enemyDistance < 8)
+        {
+            print("enemy at a medium range");
+        }
+
+        if (enemyDistance < 4)
+        {
+            print("they are getting close");
+        }
+
+    }
+
+    void EnemyDestruction()
+    {
+
+        while (enemyCount > 0)
+        {
+            print("There is an enemy !" + enemyCount);
+            enemyCount--;
+            // enemyCount = enemyCount - 1;
+        }
+    }
+
+    void EnemyRoster()
+    {
+        enemies[0] = "Orc";
+        enemies[1] = "Dragon";
+        enemies[2] = "Dinosaur";
+        enemies[3] = "Stegausorus";
+        enemies[4] = "Tricerotops";
+        enemies[5] = "Alosorus";
+
+        foreach (string enemy in enemies)
+        {
+            print(enemy);
+        }
+    }
+
+
+
+
+}
---
+++ meerkatt/Assets/ExampleScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float time = 1.0f;
+    public float distance = 100.0f;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = distance / time;
+
+        if (speed > 110 || speed < 70)
+        {
+            print("You are breaking the law !!");
+        }
+
+        print("You are traveling at " + speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            SpeedCheck();
+        }
+    }
+    void SpeedCheck()
+    {
+        speed = distance / time;
+
+        if (speed > 110 || speed < 70)
+        {
+            print("You are breaking the law !!");
+        }
+
+        print("You are traveling at " + speed);
+    }
+}
