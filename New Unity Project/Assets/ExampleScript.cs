ab04141571be351482d0236c58b442e59e2802ac
yvonne <d0686014@o365.fcu.edu.tw>
2020-10-26 12:10:50 +0800

    version 2

---
+++ New Unity Project/Assets/ExampleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float time = 2.0f;
+    public float distance = 100.0f;
+
+        public int lives = 4;
+    public bool Alive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = distance / time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ meerkat/Assets/ExampleScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float time = 1.0f;
+    public float distance = 100.0f;
+
+    public int lives = 4;
+    public bool Alive = ture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = distance / time;
+
+        if (speed > 110 || speed < 70)
+        {
+            print("You are breaking the law !!");
+        }
+
+        print("you are traveling at " + speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        SpeedCheck();
+
+    }
+    void SpeedCheck()
+    {
+        speed = distance / time;
+
+        if (speed > 110 || speed < 70)
+        {
+            print("You are breaking the law !!");
+        }
+
+        print("you are traveling at " + speed);
+    }
+}
